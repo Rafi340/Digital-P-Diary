d1998d56c5028d32e71f03a1f8b7d6cadbcac456
Rafi340 <rafisamnan340@gmail.com>
2024-09-06 10:53:04 +0600

    configure eslint and husky

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 3000;
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
-export default app;
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+export default app
