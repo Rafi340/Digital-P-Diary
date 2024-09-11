84aa1712e2914336318a03761781ff86bc38e902
Rafi340 <rafisamnan340@gmail.com>
2024-09-12 00:23:20 +0600

    user create

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,17 @@
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-import userService from './app/modules/users/user.service'
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import userRouter from './app/modules/users/user.route';
 const app: Application = express()
 
-app.use(cors())
+app.use(cors());
 // parser
-app.use(express.json())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.urlencoded({ extended: true }))
+// Routing
+app.use('/api/v1/users',userRouter);
 
 app.get('/', async (req: Request, res: Response) => {
-  await userService.createUser({
-    id: '9999',
-    name: 'admin',
-    email: 'info@example.com',
-    role: 'admin',
-    password: '1234',
-  })
   res.send('Hello World!')
 })
-export default app
+export default app;
---
+++ src/app/modules/users/user.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import userService from "./user.service";
+
+const createUser = async (req: Request, res: Response) => {
+    try{
+        const {user} = req.body;
+        const result = await userService.createUser(user);
+        res.status(200).json({ sucess: true , message: "User created successfully", data: result});
+
+    }catch(err){
+        res.status(400).json({ sucess: false , message: "Failed to create user", error: err});
+    }
+}
+export default {
+    createUser,
+};
