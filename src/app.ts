e8cb7f2a9c5cb9b69bfa06113aa8a1c03cbcb676
Rafi340 <rafisamnan340@gmail.com>
2024-09-12 00:24:51 +0600

    create user

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-import userRouter from './app/modules/users/user.route';
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
+import userRouter from './app/modules/users/user.route'
 const app: Application = express()
 
-app.use(cors());
+app.use(cors())
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // Routing
-app.use('/api/v1/users',userRouter);
+app.use('/api/v1/users', userRouter)
 
 app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!')
 })
-export default app;
+export default app
--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,6 +1,6 @@
-import express from "express";
-import userController from "./user.controller";
-const router = express.Router();
-router.post('/create', userController.createUser );
+import express from 'express'
+import userController from './user.controller'
+const router = express.Router()
+router.post('/create', userController.createUser)
 
-export default router;
+export default router
