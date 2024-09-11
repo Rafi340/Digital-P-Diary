bcbfe1192c4616930f6dbe71160a69aa0d9c27ef
Rafi340 <rafisamnan340@gmail.com>
2024-09-12 00:25:39 +0600

    controller

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -5,13 +5,11 @@ const createUser = async (req: Request, res: Response) => {
   try {
     const { user } = req.body
     const result = await userService.createUser(user)
-    res
-      .status(200)
-      .json({
-        sucess: true,
-        message: 'User created successfully',
-        data: result,
-      })
+    res.status(200).json({
+      sucess: true,
+      message: 'User created successfully',
+      data: result,
+    })
   } catch (err) {
     res
       .status(400)
