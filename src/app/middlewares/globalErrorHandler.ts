cfa13676941241a8588678ccbf8e0985ce56ecf9
Rafi340 <rafisamnan340@gmail.com>
2024-09-18 23:27:36 +0600

    refactor

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,9 +1,9 @@
-import { NextFunction, Request, Response } from 'express';
-import { error } from 'winston';
-import config from '../../config';
-import { IGenericErrorMessage } from '../../interfaces/error';
-import ApiError from '../errors/ApiError';
-import handleValidationError from '../errors/handleValidationError';
+import { NextFunction, Request, Response } from 'express'
+import { error } from 'winston'
+import config from '../../config'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import ApiError from '../errors/ApiError'
+import handleValidationError from '../errors/handleValidationError'
 
 const globalErrorHandler = (
   err,
@@ -11,40 +11,43 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction,
 ) => {
-
-  let statusCode = 500;
+  let statusCode = 500
   let message = 'Something went wrong'
-  let errorMessages: IGenericErrorMessage[] = [];
+  let errorMessages: IGenericErrorMessage[] = []
 
-  if(err.name === 'ValidationError'){
-    const simplifiedError = handleValidationError(err);
-    statusCode = simplifiedError.statusCode;
-    message = simplifiedError.message;
-    errorMessages = simplifiedError.errorMessages;
-  }else if(error instanceof ApiError){
-    statusCode = error?.statusCode;
-    message = error?.message;
-    errorMessages = error?.message? [
-      {
-        path: '',
-        message: error?.message
-      }
-    ]:[]
-  }else if(error instanceof Error) {
-    message = error?.message;
-    errorMessages = error?.message? [
-      {
-        path: '',
-        message: error?.message
-      }
-    ]:[]
+  if (err.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
+  } else if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [
+          {
+            path: '',
+            message: error?.message,
+          },
+        ]
+      : []
   }
 
   res.status(statusCode).json({
     success: false,
     message,
     errorMessages,
-    stack: config.env !== 'production' ? err?.stack : undefined
+    stack: config.env !== 'production' ? err?.stack : undefined,
   })
   next()
 }
