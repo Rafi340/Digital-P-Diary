7fd54b16b357b7865c191a4927c1f486d7b90ed2
Rafi340 <rafisamnan340@gmail.com>
2024-09-23 23:11:16 +0600

    setup start zod

--- src/app/errors/ApiError.ts
+++ src/app/errors/ApiError.ts
@@ -1,6 +1,6 @@
 class ApiError extends Error {
   statusCode: number
-  constructor(statusCode: number, message: string | undefined, stack = '') {
+  constructor(statusCode: number, message: string , stack = '') {
     super(message)
     this.statusCode = statusCode
     if (stack) {
--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,8 +1,24 @@
 import { RequestHandler } from 'express'
+import { z } from 'zod'
 import { userService } from './user.service'
 
 const createUser: RequestHandler = async (req, res, next) => {
   try {
+    const createUserZodSchema = z.object({
+      body: z.object({
+        role: z.string({
+          required_error: 'role is required'
+        }),
+        email: z.string({
+          required_error: 'email is required'
+        }),
+        name: z.string({
+          required_error: 'name is required'
+        })
+      })
+    })
+
+    await createUserZodSchema.parseAsync(req);
     const { user } = req.body
     const result = await userService.createUser(user)
     res.status(200).json({
