75935d21aa58a8e019f55fdd880971ba75e8869c
Rafi340 <rafisamnan340@gmail.com>
2024-09-24 23:31:38 +0600

    Organize zod handler

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import validateRequest from '../../middlewares/validateRequest'
 import { UserController } from './user.controller'
+import { UserZodSchema } from './user.validation'
 const router = express.Router()
-router.post('/create', UserController.createUser)
+router.post('/create', validateRequest(UserZodSchema.createUserZodSchema) ,UserController.createUser)
 
 export const UserRoutes = router
